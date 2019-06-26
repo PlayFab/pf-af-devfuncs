29f7fc5ee79dcaeff0e3982cfb4cc8b457d6e0ce
Nima Adibpour <nima.adibpour@microsoft.com>
2019-06-26 14:00:15 -0700

    Change all instances of `vertical` to `cloud`

--- csharp/ExecuteFunction.cs
+++ csharp/ExecuteFunction.cs
@@ -25,7 +25,7 @@ namespace PlayFab.AzureFunctions
     {
         private const string DEV_SECRET_KEY = "PLAYFAB_DEV_SECRET_KEY";
         private const string TITLE_ID = "PLAYFAB_TITLE_ID";
-        private const string VERTICAL_NAME = "PLAYFAB_VERTICAL_NAME";
+        private const string CLOUD_NAME = "PLAYFAB_CLOUD_NAME";
         private static readonly HttpClient httpClient = new HttpClient();
         /// <summary>
         /// A local implementation of the ExecuteFunction feature. Provides the ability to execute an Azure Function with a local URL with respect to the host
@@ -229,11 +229,11 @@ namespace PlayFab.AzureFunctions
             {
                 sb.Append(title).Append(".");
             }
-            // Append the vertical name if applicable
-            string vertical = Environment.GetEnvironmentVariable(VERTICAL_NAME, EnvironmentVariableTarget.Process);
-            if (!string.IsNullOrEmpty(vertical))
+            // Append the cloud name if applicable
+            string cloud = Environment.GetEnvironmentVariable(CLOUD_NAME, EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(cloud))
             {
-                sb.Append(vertical).Append(".");
+                sb.Append(cloud).Append(".");
             }
             // Append base PF API address
             sb.Append("playfabapi.com");
