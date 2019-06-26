ca487c43f918899116b62ea14ae75c4f43ac10fb
Nima Adibpour <47153261+nima-ap@users.noreply.github.com>
2019-06-26 14:09:52 -0700

    Rename vertical to cloud, add failed execution check (#4)
    * Add check for not successful HTTP call to local Azure Function
    
    * Change all instances of `vertical` to `cloud`

--- csharp/ExecuteFunction.cs
+++ csharp/ExecuteFunction.cs
@@ -25,7 +25,7 @@ namespace PlayFab.AzureFunctions
     {
         private const string DEV_SECRET_KEY = "PLAYFAB_DEV_SECRET_KEY";
         private const string TITLE_ID = "PLAYFAB_TITLE_ID";
-        private const string VERTICAL_NAME = "PLAYFAB_VERTICAL_NAME";
+        private const string CLOUD_NAME = "PLAYFAB_CLOUD_NAME";
         private static readonly HttpClient httpClient = new HttpClient();
         /// <summary>
         /// A local implementation of the ExecuteFunction feature. Provides the ability to execute an Azure Function with a local URL with respect to the host
@@ -153,6 +153,11 @@ namespace PlayFab.AzureFunctions
                 sw.Stop();
                 long executionTime = sw.ElapsedMilliseconds;
 
+                if (!functionResponseMessage.IsSuccessStatusCode)
+                {
+                    throw new Exception($"An error occured while executing the target function locally: FunctionName: {execRequest.FunctionName}, HTTP Status Code: {functionResponseMessage.StatusCode}.");
+                }
+
                 // Extract the response content
                 using (var functionResponseContent = functionResponseMessage.Content)
                 {
@@ -224,11 +229,11 @@ namespace PlayFab.AzureFunctions
             {
                 sb.Append(title).Append(".");
             }
-            // Append the vertical name if applicable
-            string vertical = Environment.GetEnvironmentVariable(VERTICAL_NAME, EnvironmentVariableTarget.Process);
-            if (!string.IsNullOrEmpty(vertical))
+            // Append the cloud name if applicable
+            string cloud = Environment.GetEnvironmentVariable(CLOUD_NAME, EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(cloud))
             {
-                sb.Append(vertical).Append(".");
+                sb.Append(cloud).Append(".");
             }
             // Append base PF API address
             sb.Append("playfabapi.com");
